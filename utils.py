abb2b5a274cf6acaa26a3c0d2a4447e14bd7787b
sofiavaldivieso <37985892+sofiavaldivieso@users.noreply.github.com>
2021-09-30 15:12:21 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import keras
 from keras import callbacks
 from Modelos_TL_codes.load_data import load_data
-
+import pandas as pd
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
