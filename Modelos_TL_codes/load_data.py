79646c60708e3d49d69630b470ab536132a626db
sofiavaldivieso <37985892+sofiavaldivieso@users.noreply.github.com>
2021-10-12 17:18:56 -0300

    Update load_data.py

--- Modelos_TL_codes/load_data.py
+++ Modelos_TL_codes/load_data.py
@@ -4,57 +4,37 @@ import os
 import numpy as np
 
 
-def load_data(base_dir, IMG_SIZE):
+def load_data(base_dir, IMG_SIZE, seed):
     BATCH_SIZE = 16
     train_dir = os.path.join(base_dir, 'train')
     test_dir = os.path.join(base_dir, 'test')
     val_dir = os.path.join(base_dir, 'val')
     train_dataset = image_dataset_from_directory(train_dir,
                                                  shuffle=True,
+                                                 validation_split=0.3,
+                                                 subset="training",
+                                                 seed= seed,
+                                                 label_mode='binary',
                                                  batch_size=BATCH_SIZE,
                                                  image_size=IMG_SIZE)
     class_names = train_dataset.class_names
 
     validation_dataset = image_dataset_from_directory(test_dir,
                                                       shuffle=True,
+                                                      validation_split=0.3,
+                                                      subset="validation",
+                                                      seed= seed,
+                                                      label_mode='binary',
                                                       batch_size=BATCH_SIZE,
                                                       image_size=IMG_SIZE)
-
-    test_dataset = image_dataset_from_directory(test_dir,
-                                                batch_size=128,
-                                                image_size=IMG_SIZE)    
     AUTOTUNE = tf.data.experimental.AUTOTUNE
 
     train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
     validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
-    test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
-    return train_dataset, test_dataset, validation_dataset, class_names
+    return train_dataset, validation_dataset, class_names
 
 
-def load_data_fusion(base_dir, IMG_SIZE):
-    train_dir = os.path.join(base_dir, 'train')
-    test_dir = os.path.join(base_dir, 'test')
-    val_dir = os.path.join(base_dir, 'val')
-    BATCH_SIZE = 16
-    train_dataset = image_dataset_from_directory(train_dir,
-                                                 shuffle=False,
-                                                 batch_size=BATCH_SIZE,
-                                                 image_size=IMG_SIZE)
-    class_names = train_dataset.class_names
-
-    validation_dataset = image_dataset_from_directory(test_dir,
-                                                      shuffle=False,
-                                                      batch_size=BATCH_SIZE,
-                                                      image_size=IMG_SIZE)
-
-    test_dataset = image_dataset_from_directory(test_dir,
-                                                batch_size=BATCH_SIZE,
-                                                image_size=IMG_SIZE)
-    
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-    train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
-    validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
-    test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+def load_data_fusion(base_dir, IMG_SIZE, seed):
+    train_dataset, validation_dataset, class_names = load_data(base_dir, IMG_SIZE, seed)
     return train_dataset, test_dataset, validation_dataset, class_names
 
