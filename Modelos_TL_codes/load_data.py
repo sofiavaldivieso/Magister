0098a02c2ac129bc4e9455e9ba7f7625723a29bd
sofiavaldivieso <37985892+sofiavaldivieso@users.noreply.github.com>
2021-10-12 22:04:26 -0300

    Update load_data.py

--- Modelos_TL_codes/load_data.py
+++ Modelos_TL_codes/load_data.py
@@ -5,13 +5,14 @@ import numpy as np
 
 
 def load_data(base_dir, IMG_SIZE, seed, metodologia):
+    class_names = ['ARE', 'NORMAL']
     BATCH_SIZE = 16
     train_dataset = image_dataset_from_directory(base_dir,
                                                  shuffle=True,
                                                  validation_split=0.3,
                                                  subset="training",
                                                  seed= seed,
-                                                 label_mode='binary',
+                                                 class_names=class_names,
                                                  batch_size=BATCH_SIZE,
                                                  image_size=IMG_SIZE)
     class_names = train_dataset.class_names
@@ -21,14 +22,14 @@ def load_data(base_dir, IMG_SIZE, seed, metodologia):
                                                       validation_split=0.3,
                                                       subset="validation",
                                                       seed= seed,
-                                                      label_mode='binary',
+                                                      class_names=class_names,
                                                       batch_size=BATCH_SIZE,
                                                       image_size=IMG_SIZE)
     if metodologia=='Metodologia 1' or metodologia=='Metodologia 2':
       train_dataset2 = image_dataset_from_directory('/content/drive/MyDrive/MAGISTER/DATA/OCT_Kermany',
                                                  shuffle=True,
                                                  seed= seed,
-                                                 label_mode='binary',
+                                                 class_names=class_names,
                                                  batch_size=16,
                                                  image_size=IMG_SIZE)
       train_dataset=train_dataset.concatenate(train_dataset2)
