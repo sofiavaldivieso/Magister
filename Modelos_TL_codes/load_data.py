968d1dd4f07ab4c092e85fd6d222b5877fe098a7
sofiavaldivieso <37985892+sofiavaldivieso@users.noreply.github.com>
2021-11-15 16:44:07 -0300

    Update load_data.py

--- Modelos_TL_codes/load_data.py
+++ Modelos_TL_codes/load_data.py
@@ -38,6 +38,26 @@ def load_data(base_dir, IMG_SIZE, seed, metodologia):
     validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
     return train_dataset, validation_dataset, class_names
 
+def final_load_data(base_dir, IMG_SIZE, metodologia):
+    BATCH_SIZE = 16
+    if metodologia=='Metodologia 1' or metodologia=='Metodologia 2':
+        train_dir= '/content/drive/MyDrive/MAGISTER/DATA/Dataset propio 2021/DS_Merged_OCT/train'
+        train_dataset = image_dataset_from_directory(os.path.join(train_dir, 'train'),
+                                                 shuffle=True,
+                                                 batch_size=BATCH_SIZE,
+                                                 image_size=IMG_SIZE)
+    
+    else:
+        train_dataset = image_dataset_from_directory(base_dir,
+                                                 shuffle=True,
+                                                 batch_size=BATCH_SIZE,
+                                                 image_size=IMG_SIZE)
+    
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    class_names = train_dataset.class_names
+    train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+    return train_dataset, class_names
+
 
 def load_data_fusion(base_dir, IMG_SIZE, seed):
     train_dataset, validation_dataset, class_names = load_data(base_dir, IMG_SIZE, seed)
