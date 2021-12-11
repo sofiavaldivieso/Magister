0954f5eeacda013d98f3bd102b7eb04e71365979
sofiavaldivieso <37985892+sofiavaldivieso@users.noreply.github.com>
2021-12-11 17:43:31 -0300

    Update load_data.py

--- Modelos_TL_codes/load_data.py
+++ Modelos_TL_codes/load_data.py
@@ -6,31 +6,20 @@ import numpy as np
 
 def load_data(base_dir, IMG_SIZE, seed, metodologia):
     BATCH_SIZE = 16
-    if metodologia=='Metodologia 1' or metodologia=='Metodologia 2':
-        train_dataset = image_dataset_from_directory(os.path.join(base_dir, 'train'),
-                                                 shuffle=True,
-                                                 batch_size=BATCH_SIZE,
-                                                 image_size=IMG_SIZE)
-        validation_dataset = image_dataset_from_directory(os.path.join(base_dir, 'val'),
+    train_dataset = image_dataset_from_directory(base_dir,
+                                             shuffle=True,
+                                             validation_split=0.3,
+                                             subset="training",
+                                             seed= seed,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE)
+    validation_dataset = image_dataset_from_directory(base_dir,
                                                   shuffle=True,
+                                                  validation_split=0.3,
+                                                  subset="validation",
+                                                  seed= seed,
                                                   batch_size=BATCH_SIZE,
                                                   image_size=IMG_SIZE)
-    
-    else:
-        train_dataset = image_dataset_from_directory(base_dir,
-                                                 shuffle=True,
-                                                 validation_split=0.3,
-                                                 subset="training",
-                                                 seed= seed,
-                                                 batch_size=BATCH_SIZE,
-                                                 image_size=IMG_SIZE)
-        validation_dataset = image_dataset_from_directory(base_dir,
-                                                      shuffle=True,
-                                                      validation_split=0.3,
-                                                      subset="validation",
-                                                      seed= seed,
-                                                      batch_size=BATCH_SIZE,
-                                                      image_size=IMG_SIZE)
     
     AUTOTUNE = tf.data.experimental.AUTOTUNE
     class_names = train_dataset.class_names
